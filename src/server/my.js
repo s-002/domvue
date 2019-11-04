222c69ea9374952fc21ef18dc2571f271c53cf63
s-002 <s-002>
2019-11-04 21:29:10 +0800

    rf

--- src/server/my.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export let register=params=>{
-    return axios.post('/api/user/register',params);
-}
-
-export let login=params=>{
-    return axios.post('/api/user/login',params);
-}
